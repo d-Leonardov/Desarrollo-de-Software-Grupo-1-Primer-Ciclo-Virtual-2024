e3e7df9dc102db85aed2c2a6550f1224d3ae6dac
Leonardo Villalobos <leonardov212@gmail.com>
2024-04-14 17:06:56 -0400

    Guia4

--- ProyectoFinal/src/ConexionBaseDeDatos.java
+++ ProyectoFinal/src/ConexionBaseDeDatos.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBaseDeDatos {
-    private static final String URL = "jdbc:mysql://localhost:3306/universidad";
-    private static final String USUARIO = "root";
-    private static final String PASSWORD = "Leonardo3839332";
+    private static final String URL = "jdbc:mysql://localhost:3306/universidad"; // Cambiar el
+    private static final String USUARIO = "root"; // Cambiar el usuario
+    private static final String PASSWORD = "Leonardo3839332"; // Cambiar la contraseña del usuario
 
     public static Connection conectarBD() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, PASSWORD);
